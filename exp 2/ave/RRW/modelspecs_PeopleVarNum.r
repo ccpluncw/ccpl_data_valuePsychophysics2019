5084a8757f49f0181a4280191fbb115538ed2778
DJC <dalejcohen@gmail.com>
2022-04-23 10:52:43 -0400

    added exp 2

---
+++ exp 2/ave/RRW/modelspecs_PeopleVarNum.r
@@ -0,0 +1,222 @@
+###############################
+#### MODEL SPECIFICATIONS #####
+###############################
+
+
+
+#in this model: positive coefficients for the "s" parameter indicate a bias toward the "keep" boundary and negative coefficients indicate a bias toward the "donate" boundary
+grpVars <- c("defaultKilled","peopleQuantity")
+
+#fixedNSD
+#Add NSD as a fixed parameter (NSD = 1)
+#Here I group by the three variables but add not effects for them.  This will split the data properly
+  columnName <- NULL
+  df.code <- NULL
+  GroupByVariables <- grpVars
+  parameter <- "nSD"
+  ParameterName <- "nSD"
+  parameterBounds <- c(1, 1, 0.25)
+
+  fixedNSD  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+#freeNSD
+#Add NSD as a fixed parameter (NSD = 1)
+#Here I group by the three variables but add not effects for them.  This will split the data properly
+  columnName <- NULL
+  df.code <- NULL
+  GroupByVariables <- grpVars
+  parameter <- "nSD"
+  ParameterName <- "nSD"
+  parameterBounds <- c(7, 1, 0.25)
+
+  freeNSD  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+  #freeDB
+  #Add DB as a free parameter
+  columnName <- NULL
+  df.code <- NULL
+  GroupByVariables <- NULL
+  parameter <- "db"
+  ParameterName <- "db"
+  parameterBounds <- c(0.5, 0, 0.001)
+
+  freeDB  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+  #freeB
+  #Add B as a free parameter
+  #add a column for the fixed parameter.  This is needed because we will be adding another b effect later
+    x1 <- "default"
+    #when the leftItem is HV0 then bias towards the lowerBound (choose LVO: bias = right Item)
+    v1 <- 1
+
+  columnName <- "bConstant"
+  df.code <- data.frame(logic = c(x1), value = c(v1))
+  GroupByVariables <- grpVars
+  parameter <- "b"
+  ParameterName <- "bConstant"
+  parameterBounds <- c(100, 5, 1)
+
+  freeB  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+  #freeValue
+  #Add v as a fixed parameter (v = -0.07)
+  #add a column for the fixed parameter.  This is needed because we will be adding another vc effect later
+    x1 <- "default"
+    #when the leftItem is HV0 then bias towards the lowerBound (choose LVO: bias = right Item)
+    v1 <- 1
+
+    columnName <- "VCconstantColumn"
+    df.code <- data.frame(logic = c(x1), value = c(v1))
+    GroupByVariables <- grpVars
+    parameter <- "vc"
+    ParameterName <- "vConstant"
+    parameterBounds <- c(0.35, -0.35, 0.001)
+
+  freeVC <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+    #peopleQuantityOverallSE
+    #add SE effect to overallSE effect model
+      x1 <- "peopleQuantity == 'HVO_2-LVO_1'"
+      v1 <- 1
+      x2 <- "peopleQuantity == 'HVO_1-LVO_2'"
+      v2 <- -1
+      x3 <- "default"
+      v3 <- 0
+
+      columnName <- "peopleQuantitySEColumn"
+      df.code <- data.frame(logic = c(x1,x2,x3), value = c(v1,v2,v3))
+      GroupByVariables <- grpVars
+      ParameterName <- "sPeopleQuantity"
+      parameter <- "s"
+      parameterBounds <- c(0.8, -0.8, 0.001)
+
+    peopleQuantitySE  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+    #peopleQuantityVC
+    #add value change model
+      x1 <- "peopleQuantity == 'HVO_2-LVO_1'"
+      v1 <- 1
+      x2 <- "peopleQuantity == 'HVO_1-LVO_2'"
+      v2 <- -1
+      x3 <- "default"
+      v3 <- 0
+
+    columnName <- "peopleQuantityVCColumn"
+    df.code <- data.frame(logic = c(x1,x2,x3), value = c(v1,v2,v3))
+    GroupByVariables <- grpVars
+    ParameterName <- "vPeopleQuantity"
+    parameter <- "vc"
+    parameterBounds <- c(0.25, -0.25, 0.001)
+
+    peopleQuantityVC  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+    #peopleQuantityB
+    #add Boundary model
+      x1 <- "peopleQuantity == 'HVO_2-LVO_2'"
+      v1 <- 1
+      x2 <- "peopleQuantity == 'HVO_1-LVO_1'"
+      v2 <- -1
+      x3 <- "default"
+      v3 <- 0
+
+    columnName <- "peopleQuantityBColumn"
+    df.code <- data.frame(logic = c(x1,x2,x3), value = c(v1,v2,v3))
+    GroupByVariables <- grpVars
+    ParameterName <- "bPeopleQuantity"
+    parameter <- "b"
+    parameterBounds <- c(100, 0, 1)
+
+    peopleQuantityB  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+    #now predict the split of "defaultKilled" by a shift in the startpoint (s).  Do that by adding a dummy coded dataframe (df.code)
+    #Here, we input the conditional statements for coding the dummy or effect variable.  X is the conditional, V is the value.
+      x1 <- "defaultKilled == 'defaultLVO'"
+      #when the defaultKilled is LVI then bias towards the upperBound (save HVI: bias = no action)
+      v1 <- 1
+      x2 <- "defaultKilled == 'defaultHVO'"
+      #when the defaultKilled is HVI then bias towards the lowerBound (save LVI: bias = no action)
+      v2 <- -1
+      x3 <- "default"
+      v3 <- 0
+
+      #this is the columnName of the dummy/effect variable
+      columnName <- "defaultKilledSEColumn"
+      #this dataframe contains the coding inforamtion of the dummy/effect variable
+      df.code <- data.frame(logic = c(x1,x2,x3), value = c(v1,v2,v3))
+      #here we have the grouping variable(s) that will be used by the ddply to create the summary dataset.
+      GroupByVariables <- grpVars
+      #this is the name given to the parameter that will measure the effect of this dummy/effect variable
+      ParameterName <- "sDKbias"
+      #this is the parameter name for the RRW model. There are specific names: s, b, nSD, db, da, vc.
+      parameter <- "s"
+      #These are the bounds of the parameter values: c(high, low, interval)
+      parameterBounds <- c(0.8, -0.8, 0.001)
+
+    #add them to an existing model: here we add them to the simple model to create the overall Start Effect Model
+    defaultKilledSE  <- rrwCreateParameterEffect(parameter = parameter, columnName = columnName, ParameterName = ParameterName, parameterBounds = parameterBounds, df.code = df.code, GroupByVariables = GroupByVariables)
+
+#########################
+### build models
+#########################
+
+simpleModelList <- NULL
+
+#simple model
+simpleModelList <- rrwAddParameterEffectListToRRWModel(simpleModelList, c(freeNSD, freeDB, freeB))
+
+#single biases
+dkSEModelList <- rrwAddParameterEffectListToRRWModel(simpleModelList, c(defaultKilledSE))
+
+pqSEModelList <- rrwAddParameterEffectListToRRWModel(simpleModelList, c(peopleQuantitySE))
+
+pqBModelList <- rrwAddParameterEffectListToRRWModel(simpleModelList, c(peopleQuantityB))
+
+pqVCModelList <- rrwAddParameterEffectListToRRWModel(simpleModelList, c(peopleQuantityVC))
+
+#two way biases
+pqSEdkSEModelList <- rrwAddParameterEffectListToRRWModel(dkSEModelList, c(peopleQuantitySE))
+
+pqBdkSEModelList <- rrwAddParameterEffectListToRRWModel(dkSEModelList, c(peopleQuantityB))
+
+pqVCdkSEModelList <- rrwAddParameterEffectListToRRWModel(dkSEModelList, c(peopleQuantityVC))
+
+pqBpqSEModelList <- rrwAddParameterEffectListToRRWModel(pqSEModelList, c(peopleQuantityB))
+
+pqVCpqSEModelList <- rrwAddParameterEffectListToRRWModel(pqSEModelList, c(peopleQuantityVC))
+
+pqVCpqBModelList <- rrwAddParameterEffectListToRRWModel(pqBModelList, c(peopleQuantityVC))
+
+#three way biases
+pqBpqSEdkSEModelList <- rrwAddParameterEffectListToRRWModel(pqSEdkSEModelList, c(peopleQuantityB))
+
+pqVCpqSEdkSEModelList <- rrwAddParameterEffectListToRRWModel(pqSEdkSEModelList, c(peopleQuantityVC))
+
+pqVCpqBdkSEModelList <- rrwAddParameterEffectListToRRWModel(pqBdkSEModelList, c(peopleQuantityVC))
+
+pqVCpqSEpqBModelList <- rrwAddParameterEffectListToRRWModel(pqBpqSEModelList, c(peopleQuantityVC))
+
+#four way biases
+
+pqVCpqSEpqBdkSEModelList <- rrwAddParameterEffectListToRRWModel(pqBpqSEdkSEModelList, c(peopleQuantityVC))
+
+
+
+allModels <- list(simpleModelList= simpleModelList,
+  dkSEModelList = dkSEModelList,
+  pqSEModelList=pqSEModelList,
+  pqBModelList=pqBModelList,
+  pqVCModelList = pqVCModelList,
+  pqSEdkSEModelList = pqSEdkSEModelList,
+  pqBdkSEModelList = pqBdkSEModelList,
+  pqVCdkSEModelList = pqVCdkSEModelList,
+  pqBpqSEModelList = pqBpqSEModelList,
+  pqVCpqSEModelList = pqVCpqSEModelList,
+  pqVCpqBModelList = pqVCpqBModelList,
+  pqBpqSEdkSEModelList = pqBpqSEdkSEModelList,
+  pqVCpqSEdkSEModelList = pqVCpqSEdkSEModelList,
+  pqVCpqBdkSEModelList = pqVCpqBdkSEModelList,
+  pqVCpqSEpqBModelList = pqVCpqSEpqBModelList,
+  pqVCpqSEpqBdkSEModelList = pqVCpqSEpqBdkSEModelList
+)
+
+allFixedModels <- NULL
---
+++ exp 2/sum/peopleVarNumDataPrep.r
@@ -0,0 +1,74 @@
+library(chMorals)
+library(chutils)
+library(stringr)
+
+cleanOverlapFile <- FALSE
+
+#set up the new RT variables
+fitCol <- "fit.RT"
+resCol <- "res.RT"
+useTwoParameterModel <- FALSE
+overlapDataIsComplete <- TRUE
+
+# read in parameters
+params<-ch.readMoralsDBfile("moralsDBfile.txt")
+
+#set up the group and item directories
+mainDir <- getwd()
+ch.newDir (mainDir, params$gpSubDir)
+gpDir <- getwd()
+setwd(mainDir)
+
+ch.newDir (mainDir, params$itemSubDir)
+itemDir <- getwd()
+setwd(mainDir)
+
+
+statsOutputFile <- file.path(mainDir,paste(params$dt.set, params$statsOutputFilePrefix))
+sink(statsOutputFile, append = F)
+  cat("\n***** New Run ****\n\n")
+sink(NULL)
+
+
+### read in data
+data.raw <-read.table(params$moralsTaskDataFile, header=T, sep="\t")
+data.ovrlp <-read.table(params$valueOverlapDataFile, header=T, sep="\t", quote="\"")
+
+######_____REMOVE PRACTICE TRIALS _____######
+data.raw <- data.raw[data.raw$trial_type >=1, ]
+
+### do Prep analysis
+processedData <- ch.moralsDataPrep(data.raw, "sn", "keybRT", "overlap", "direction", "trial", "keyDef", respChoiceVal = c("Yes", "No"), item1cols = c("IA.1", "IA.2"), item2cols = c("IB.1", "IB.2"), overlapItem1cols = c("IA1", "IA2"), overlapItem2cols = c("IB1", "IB2"), statsOutputFile = statsOutputFile, params = params, overlapDataIsComplete = overlapDataIsComplete)
+
+### get HVO quantity
+processedData$HVOq <- ifelse((processedData$QuantOption1 == 2 & processedData$direct.xVy == 1) | (processedData$QuantOption2 == 2 & processedData$direct.xVy == -1) , 2, 1)
+### get LVO quantity
+processedData$LVOq <- ifelse((processedData$QuantOption1 == 2 & processedData$direct.xVy == -1) | (processedData$QuantOption2 == 2 & processedData$direct.xVy == 1) , 2, 1)
+
+### Filter data
+analysisReadyData <- ch.moralsFilterDataQ(processedData, "sn", "keybRT", "overlapRound", "correct",c(1,0), statsOutputFile = statsOutputFile, params = params)
+
+### Do RT and p(Hit Analysis on Group Data - remove learning effects for the group)
+analysisReadyData.gp <- ch.moralsGrpRTpHit(analysisReadyData, "trial", "keybRT", fitCol, resCol, "overlapRound", "keyDef",c("Yes", "No"), "correct",c(1,0), useTwoParameterModel = useTwoParameterModel, params = params)
+write.table(analysisReadyData.gp, file="analysisReadyData.gp.txt", append=FALSE, quote=FALSE, sep="\t", row.names=FALSE, col.names=TRUE)
+
+### Do RT and p(Hit Analysis on individual subject Data - remove learning effects for each subject)
+analysisReadyData.sn <- ch.moralsSnRTpHit(analysisReadyData, "sn", "trial", "keybRT", fitCol, resCol, "overlap", "correct", c(1,0),  useTwoParameterModel = useTwoParameterModel, params = params, minUniqueOverlaps = 4)
+write.table(analysisReadyData.sn, file="analysisReadyData.sn.txt", append=FALSE, quote=FALSE, sep="\t", row.names=FALSE, col.names=TRUE)
+
+#Do d'analysis as a group, but use the data whereby the learning effects were removed by subject
+df.dPrime <- ch.moralsDprimeAnalysis(analysisReadyData.sn, "overlapRound", "correct", c(1,0), "targetPresent", c(TRUE,FALSE), resCol, params = params, filenameID = "gp")
+write.table(df.dPrime, file="df.dPrime.txt", append=FALSE, quote=FALSE, sep="\t", row.names=FALSE, col.names=TRUE)
+
+
+#### For experiments with catagory variable manipulations (e.g., different groups), do an analysis
+#### by group
+#### this one is by side
+    grpFitModels <- ch.moralsPlotsByGrpsAndGetModels(analysisReadyData.gp, c("QuantOption1", "QuantOption2"), resCol, "overlapRound", "keyDef", yesNoVal = c("Yes", "No"), "correct", c(1,0), "targetPresent", c(TRUE,FALSE), useTwoParameterModel = TRUE, params = params, minNperOverlap = 0)
+    #### this one is by side
+    grpFitModels.2 <- ch.moralsPlotsByGrpsAndGetModels(analysisReadyData.gp, c("HVOq", "LVOq"), resCol, "overlapRound", "keyDef", yesNoVal = c("Yes", "No"), "correct", c(1,0), "targetPresent", c(TRUE,FALSE), useTwoParameterModel = TRUE, params = params, minNperOverlap = 0)
+    ### and plot the data
+    setwd(gpDir)
+    ch.moralsPlotFitsByGrps(grpFitModels, c("QuantOption1", "QuantOption2"), "overlapRound", analysisReadyData.gp, filenameID = params$dt.set)
+    ch.moralsPlotFitsByGrps(grpFitModels.2, c("HVOq", "LVOq"), "overlapRound", analysisReadyData.gp, filenameID = params$dt.set)
+    setwd(mainDir)
